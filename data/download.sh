41bad28c4ff598a137a498c0d5b9708f7fa95cc8
khanhptnk <nguyenxuankhanhm@gmail.com>
2018-12-16 12:17:48 -0500

    update
    Former-commit-id: 1b5c450f7bf6b6c0d1da96ee220e5de3f9309fb9
    Former-commit-id: 5a2479caee597191dadb10887ec535502760badd [formerly b5e901e514abb299f821282aafff11538f7eccbd]
    Former-commit-id: a1e17a99a5729c460247f62e3ceb1d00bed6b518

--- data/download.sh
+++ data/download.sh
@@ -2,3 +2,10 @@
 
 wget -O vnla_data.zip https://www.dropbox.com/s/06ilmo55w4a5mh8/vnla_data.zip?dl=1
 unzip vnla_data.zip
+
+mkdir img_features
+cd img_features
+wget https://storage.googleapis.com/bringmeaspoon/img_features/ResNet-152-imagenet.zip
+unzip ResNet-152-imagenet.zip
+
+
