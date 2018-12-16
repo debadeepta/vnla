3b391bd3c511a682ba1015ccfa42975e71833c18
khanhptnk <nguyenxuankhanhm@gmail.com>
2018-12-16 11:48:48 -0500

    update
    Former-commit-id: f9a6307dc0b062f41c6c199cc4a94c43449f4533
    Former-commit-id: a33b51eb6867128a1983e8126f28f6de685c390e [formerly f3e2c694b88bf0ec50cf2e0c775ecec30458745a]
    Former-commit-id: 979c39f55a1af7bae049084ef1fb2a7828a06908

--- data/download.sh
+++ data/download.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget -O vnla_data.zip https://www.dropbox.com/s/k7553po88dhszr8/vnla_data.zip?dl=1
+wget -O vnla_data.zip https://www.dropbox.com/s/06ilmo55w4a5mh8/vnla_data.zip?dl=1
 unzip vnla_data.zip
