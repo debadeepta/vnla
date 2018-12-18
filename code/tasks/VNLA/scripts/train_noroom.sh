da49f63b941abf9865c1351440183d6cd6e361db
khanhptnk <nguyenxuankhanhm@gmail.com>
2018-12-17 21:30:16 -0500

    update
    Former-commit-id: c0b822467092add0edee8925119d1aed690ddd3c
    Former-commit-id: 20bcd755c911971e4ae2735b7fd9fa998eec6209 [formerly 633cf01e9bea1e01f575fdcb11033d0777ed681c]
    Former-commit-id: 2f4bbaede6f03ad5ff11c25bf8c910b9c8fc1e36

--- code/tasks/VNLA/scripts/train_noroom.sh
+++ code/tasks/VNLA/scripts/train_noroom.sh
@@ -8,7 +8,7 @@ exp_name=$1
 device=${2:-0}
 
 config_file="configs/verbal_hard_noroom.json"
-output_dir="subgoal_effects_$exp_name"
+output_dir="${exp_name}"
 
 command="python train.py -config $config_file -exp $output_dir"
 
