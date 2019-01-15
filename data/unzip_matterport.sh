788a21da222d9bb79278c00a48bbd020233200f0
khanhptnk <nguyenxuankhanhm@gmail.com>
2019-01-15 10:12:44 +0700

    unzip matterport script
    Former-commit-id: 48ee723adb768933717763f74b3af21c53ed23cb
    Former-commit-id: 5a9420ff7298f7317883068dc332a823c2c9df9f [formerly 2e11c039aef49caf9581f4acfcaaee2ef321568c]
    Former-commit-id: 9ad3e4e559d008bde12acef42f5a2b7fe0a9874f

---
+++ data/unzip_matterport.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+matter_root=$1
+
+if [ ! -d "$matter_root" ]; then
+  echo "Usage: bash unzip_matterport.zip matter_root"
+  echo "matter_root is the Matterport3D dataset folder where matter_root/v1/scans is located"
+  exit 1
+fi
+
+cd $matter_root/v1/scans
+
+miss_hs=0
+miss_sb=0
+for folder in *; do
+  filename="$folder/house_segmentations.zip"
+  if [ ! -f $filename ]; then
+    echo "$filename does not exist!"
+    let miss_hs++
+  else
+    unzip $filename -d .
+  fi
+
+  filename="$folder/matterport_skybox_images.zip"
+  if [ ! -f $filename ]; then
+    echo "$filename does not exist!"
+    let miss_sb++
+  else
+    unzip $filename -d .
+  fi
+done
+
+echo ""
+echo "Done! Missing $miss_hs house_segmentations zip files and $miss_sb matterport_skybox_images zip files"
