365df5e48fe2e2f8c6f0e0ec56e7d9901fa1bdf9
khanhptnk <nguyenxuankhanhm@gmail.com>
2019-03-17 13:57:57 -0400

    update
    Former-commit-id: a856e27312c1f70834e1dd2962f01a17c4fad55f
    Former-commit-id: d00c6576ec64567700060e92d3c3a1b0068d25d7 [formerly cadcafd4a57a31a7bdba3d7b45c0111d666ee18e]
    Former-commit-id: 7541e78171a383fe04a79762a62062c35bb9e493

--- data/download.sh
+++ data/download.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+git clone --recursive https://github.com/peteanderson80/Matterport3DSimulator.git
+cp -r Matterport3DSimulator/connectivity . 
+yes | rm -rf Matterport3DSimulator
+
 wget -O vnla_data.zip https://www.dropbox.com/s/06ilmo55w4a5mh8/vnla_data.zip?dl=1
 unzip vnla_data.zip
 
 mkdir img_features
 cd img_features
-wget https://storage.googleapis.com/bringmeaspoon/img_features/ResNet-152-imagenet.zip
+wget https://www.dropbox.com/s/715bbj8yjz32ekf/ResNet-152-imagenet.zip?dl=1
 unzip ResNet-152-imagenet.zip
 
 
